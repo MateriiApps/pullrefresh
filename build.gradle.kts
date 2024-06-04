a0056848348207bc51ad4a425c96ce39a0092efc
Tom Pratt <tom.pratt@outlook.com>
2024-06-04 16:29:55 +0200

    Bump dependencies (#13)
    The compose multiplatform version needed to be bumped to be compatible with projects using kotlin 2.0.0 and related compose versions. Bump other dependencies at the same time.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
 
 allprojects {
     group = "dev.materii.pullrefresh"
-    version = "1.4.0-beta02"
+    version = "1.4.0-beta03"
 
     repositories {
         repositories {
--- pullrefresh/build.gradle.kts
+++ pullrefresh/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.multiplatform)
     id("maven-publish")
     id("signing")
