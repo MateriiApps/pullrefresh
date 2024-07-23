7492b4b72cad5367b59a36185b10686849a01e29
LakeLab <todaypoc@gmail.com>
2024-07-23 20:42:33 +0900

    [#15] Upward scroll fling value not consumed when refresh callback Is called
    Consuming the fling value when the refresh starts since it holds no significant meaning in this context.
    
    https://github.com/MateriiApps/pullrefresh/issues/15

--- demo/src/main/java/dev/materii/pullrefresh/demo/sample/DragRefreshSample.kt
+++ demo/src/main/java/dev/materii/pullrefresh/demo/sample/DragRefreshSample.kt
@@ -1,23 +1,26 @@
 package dev.materii.pullrefresh.demo.sample
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import dev.materii.pullrefresh.DragRefreshLayout
+import androidx.compose.ui.unit.dp
 import dev.materii.pullrefresh.DragRefreshIndicator
+import dev.materii.pullrefresh.DragRefreshLayout
 import dev.materii.pullrefresh.PullRefreshState
 
 @Composable
 fun DragRefreshSample(
     flipped: Boolean,
     pullRefreshState: PullRefreshState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isRefreshing: Boolean
 ) {
     DragRefreshLayout(
         modifier = modifier,
@@ -31,13 +34,22 @@ fun DragRefreshSample(
             )
         }
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            contentAlignment = Alignment.Center
+        LazyColumn(
+            Modifier.fillMaxSize(),
+            userScrollEnabled = !isRefreshing
         ) {
-            Text(text = "Pull ${if (flipped) "up" else "down"} to refresh")
+            items(100) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.outline,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .padding(10.dp), text = "No. $it"
+                )
+            }
         }
     }
 }
