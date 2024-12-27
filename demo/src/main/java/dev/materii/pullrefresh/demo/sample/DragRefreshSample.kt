0deeec24bef8e439d5c4117b9b7dfecc65d1d41b
Lake <15243641+LakeLab@users.noreply.github.com>
2024-12-28 03:29:20 +0900

    Fix: Accept the feedback for the code style and rollback the sample code to match with README.md (#19)
    * A one-liner arrow for a multiline if seems weird, using blocks instead
        * https://github.com/MateriiApps/pullrefresh/pull/16#discussion_r1705263375
    * Add a whitespace
        * https://github.com/MateriiApps/pullrefresh/pull/16#discussion_r1705263692
    * Change back DragRefreshSample.kt code to match with README.md

--- demo/src/main/java/dev/materii/pullrefresh/demo/sample/DragRefreshSample.kt
+++ demo/src/main/java/dev/materii/pullrefresh/demo/sample/DragRefreshSample.kt
@@ -1,16 +1,14 @@
 package dev.materii.pullrefresh.demo.sample
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import dev.materii.pullrefresh.DragRefreshIndicator
 import dev.materii.pullrefresh.DragRefreshLayout
 import dev.materii.pullrefresh.PullRefreshState
@@ -19,8 +17,7 @@ import dev.materii.pullrefresh.PullRefreshState
 fun DragRefreshSample(
     flipped: Boolean,
     pullRefreshState: PullRefreshState,
-    modifier: Modifier = Modifier,
-    isRefreshing: Boolean
+    modifier: Modifier = Modifier
 ) {
     DragRefreshLayout(
         modifier = modifier,
@@ -34,22 +31,13 @@ fun DragRefreshSample(
             )
         }
     ) {
-        LazyColumn(
-            Modifier.fillMaxSize(),
-            userScrollEnabled = !isRefreshing
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            contentAlignment = Alignment.Center
         ) {
-            items(100) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(5.dp)
-                        .background(
-                            color = MaterialTheme.colorScheme.outline,
-                            shape = RoundedCornerShape(10.dp)
-                        )
-                        .padding(10.dp), text = "No. $it"
-                )
-            }
+            Text(text = "Pull ${if (flipped) "up" else "down"} to refresh")
         }
     }
 }
